bf871e14b3f729906282bb5861a58f44989b7963
Boon Heng <bhenggoh@gmail.com>
2022-11-26 12:11:33 +0800

    Testing

--- scripts/gettable.js
+++ scripts/gettable.js
@@ -3,22 +3,34 @@ const startdate = document.getElementById("startdate");
 const enddate = document.getElementById("enddate");
 
 function getDataFromApi(event) {
+
   // GBH
-  // Used the new URL
+  // To run from my Netlify: https://meek-snickerdoodle-4d6567.netlify.app/
+
+  // GBH
+  // This is the original URL. This works.
   // let url = "https://fe04156e-b0a5-468a-a569-dab0a548bb56.mock.pstmn.io";
+  // This is my own POSTMAN URL. This works. (Make sure is JSON)
   // let url = "https://3ffc337b-df38-4f02-b3de-85e05e08e2e9.mock.pstmn.io";
 
-
+  // This is Lex's production URL (same as POSTMAN I think)
   let url = "https://nus-course-backend-5i7iu7m5xa-uc.a.run.app";
 
   var settings = {
     // GBH
-    // Used the new URL
-    //  url: `${url}/transactions?startdate=${startdate}&enddate=${enddate}`,
+    // This is the original API. It works.
+    // url: `${url}/transactions?startdate=${startdate}&enddate=${enddate}`,
 
-    //url: `${url}/transaction?startDate=2022-09-10&endDate=2022-10-10&category=food&description=coffee`,
-    //url: `${url}/transactions`,
-    url: `${url}/user`,
+    // This is my API using POSTMAN. It works.
+    // url: `${url}/transactions`,
+
+    // This is Lex's API. I just hardcode the param for now.
+    // Error in console -- something about CORS.
+    url: `${url}/transaction?startDate=2022-09-10&endDate=2022-10-10&category=food&description=coffee`,
+    
+    // I also tried with a simpler API. Also not working. Same CORS error.
+    //url: `${url}/user`,
+
     method: "GET",
     timeout: 0,
     headers: {
@@ -30,10 +42,9 @@ function getDataFromApi(event) {
   console.log("calling my new API");
 
   $.ajax(settings).done(function (response) {
-    myArray = response;
     console.log("INSIDE");
+    myArray = response;
     console.log(myArray);
-    console.log("INSIDE1");
     buildTable(myArray);
   });
   event.preventDefault();
