2fc42dfe6df592ea7372b76959061c6ead3ca80c
Jia Hui <ngjiahui09@gmail.com>
2022-11-26 11:23:37 +0800

    jiahui minor changes

---
+++ planFunctions.js
@@ -0,0 +1,17 @@
+function createPlanButton() {
+
+    const food = document.getElementById("food");
+    const transport = document.getElementById("transport");
+    const utilities = document.getElementById("utilities");
+    const shopping = document.getElementById("shopping");
+    const misc = document.getElementById("misc");
+
+    //https://www.freecodecamp.org/news/how-to-make-api-calls-with-fetch/
+
+    window.location.href = 'createPlanSuccess.html';
+  }
+
+
+
+
+
