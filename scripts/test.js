fe0ef98adec5aad9543a8de86f0c84b9473e5e34
lexxalsia <sykang_8@msn.com>
2022-11-27 08:55:07 +0800

    Add new test button for internal /User API

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,17 @@
 const b = document.getElementById("getusers");
+const c = document.getElementById("getuser");
 
 b.addEventListener("click", async () => {
   fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/users", {
+    method: "GET",
+    mode: "cors",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+  }).then((response) => response.json());
+});
+
+c.addEventListener("click", async () => {
+  fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/user", {
     method: "GET",
     mode: "cors",
     headers: { "Content-Type": "application/json" },
