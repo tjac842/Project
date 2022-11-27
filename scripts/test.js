c3ccecf13b8e918769f9dd5443b00c9d1c2f3200
lexxalsia <sykang_8@msn.com>
2022-11-28 00:18:16 +0800

    Update examples

--- scripts/test.js
+++ scripts/test.js
@@ -1,18 +1,36 @@
-const b = document.getElementById("getusers");
-const c = document.getElementById("getuser");
+const getUserButton = document.getElementById("getuser");
+const addPlanButton = document.getElementById("addplan");
 
-b.addEventListener("click", async () => {
-  fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/users", {
-    method: "GET",
-    mode: "cors",
-    headers: { "Content-Type": "application/json" },
-  }).then((response) => response.json());
-});
-
-c.addEventListener("click", async () => {
+getUserButton.addEventListener("click", async () => {
   fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/user", {
     method: "GET",
-    mode: "cors",
     headers: { "Content-Type": "application/json" },
-  }).then((response) => response.json());
+    mode: "cors",
+    credentials: "include",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data || {};
+    })
+    .catch((error) => console.log(error));
+});
+
+addPlanButton.addEventListener("click", async () => {
+  fetch("http://localhost:3000/Plan", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    mode: "cors",
+    credentials: "include",
+    body: JSON.stringify({
+      name: "Escape Plan DDDDD",
+      settings: '[{ "Category": "Utilities", "Value":0 }]',
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      return data || {};
+    })
+    .catch((error) => console.log(error));
 });
