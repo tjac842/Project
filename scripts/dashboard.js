2718903c43d47e2a30e3f64c89a8eed78eb2c708
lexxalsia <sykang_8@msn.com>
2022-11-30 11:01:00 +0800

    Update dashboard.html and css.
    Link up dashboard with overview API

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,39 +1,95 @@
 const connect = document.getElementById("connect_btn");
 const balance = document.getElementById("balance");
-
+const container = document.getElementById("dashboard_container");
 
 connect.addEventListener("click", async () => {
-    fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/account/connect", {
-      method: "GET",
-      mode: "cors",
-      headers: { "Content-Type": "application/json" },
-      credentials: "include",
+  fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/account/connect", {
+    method: "GET",
+    mode: "cors",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data || {};
     })
-        .then((response) => response.json())
-        .then((data) => {
-            return data || {};
-          })
-          .catch((error) => console.log(error));
+    .catch((error) => console.log(error));
 });
 
 //Balance
-fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/account/balance")
-  .then(response => response.json())
-  .then(response => {
-    res.data.map(user => {
-      console.log(`$${balance}`)
-    });
-});
+fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/account/balance", {
+  method: "GET",
+  mode: "cors",
+  headers: { "Content-Type": "application/json" },
+  credentials: "include",
+})
+  .then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    }
+  })
+  .then((data) => {
+    if (data !== undefined) {
+      balance.innerHTML = `SGD $ ${data.Balance.toFixed(2)}`;
+    }
+  });
 
-// balance.addEventListener("click", async () => {
-//     fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/account/balance", {
-//       method: "GET",
-//       mode: "cors",
-//       headers: { "Content-Type": "application/json" },
-//       credentials: "include",
-//     }).then((response) => response.json())
-//     .then((data) => {
-//                 return data || {};
-//               })
-//               .catch((error) => console.log(error));
-//   });
+//Overview
+fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/overview", {
+  method: "GET",
+  mode: "cors",
+  headers: { "Content-Type": "application/json" },
+  credentials: "include",
+})
+  .then((response) => {
+    if (response.status === 200) {
+      return response.json();
+    }
+  })
+  .then((data) => {
+    if (Array.isArray(data) && data.length > 0) {
+      data.map((cat) => {
+        const catBox = document.createElement("div");
+
+        if (cat.Target === 0) {
+          catBox.className = "dashboard_cat_box";
+        } else {
+          const spent = cat.Sum / cat.Target;
+
+          if (spent < 0.8) {
+            catBox.className = "dashboard_healthy_cat_box";
+          } else if (spent < 1.0) {
+            catBox.className = "dashboard_danger_cat_box";
+          } else {
+            catBox.className = "dashboard_unhealthy_cat_box";
+          }
+        }
+
+        const catTitle = document.createElement("div");
+        catTitle.className = "cat_title";
+
+        const title = document.createElement("p");
+        title.appendChild(document.createTextNode(cat.Category));
+        catTitle.appendChild(title);
+
+        const value = document.createElement("p");
+        value.appendChild(
+          document.createTextNode(`$ ${cat.Sum} / ${cat.Target}`)
+        );
+
+        catTitle.appendChild(value);
+        catBox.appendChild(catTitle);
+        container.appendChild(catBox);
+      });
+    } else {
+      container.appendChild(
+        document
+          .createElement("div")
+          .appendChild(
+            document.createTextNode(
+              "Opps.. It's empty. Connect to your account to start."
+            )
+          )
+      );
+    }
+  });
--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,6 @@
 const getUserButton = document.getElementById("getuser");
 const addPlanButton = document.getElementById("addplan");
+const getOverviewButton = document.getElementById("getoverview");
 
 getUserButton.addEventListener("click", async () => {
   fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/user", {
@@ -30,6 +31,20 @@ addPlanButton.addEventListener("click", async () => {
     .then((data) => {
       console.log(data);
 
+      return data || {};
+    })
+    .catch((error) => console.log(error));
+});
+
+getOverviewButton.addEventListener("click", async () => {
+  fetch("https://nus-course-backend-5i7iu7m5xa-uc.a.run.app/account/overview", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+    mode: "cors",
+    credentials: "include",
+  })
+    .then((response) => response.json())
+    .then((data) => {
       return data || {};
     })
     .catch((error) => console.log(error));
